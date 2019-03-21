4b6113a7e9c3b441d376b106fdd93bdacf3affa3
undefined <reissedler@gmail.com>
2019-03-21 20:25:03 +0000

    a

--- draughts.js
+++ draughts.js
@@ -28,7 +28,7 @@ var multiplier = 1;
 var tableLimit,reverse_tableLimit ,  moveUpLeft,moveUpRight, moveDownLeft,moveDownRight , tableLimitLeft, tableLimitRight;
 
 	getDimension();
-	if(windowWidth > 640){
+	if (windowWidth > 640){
 		moveLength = 80;
 		moveDeviation = 10;
 	}
@@ -53,7 +53,7 @@ var checker = function(piece,color,square) {
 	this.ocupied_square = square;
 	this.alive = true;
 	this.attack = false;
-	if(square%8){
+	if (square%8){
 		this.coordX= square%8;
 		this.coordY = Math.floor(square/8) + 1 ;
 	}
@@ -79,11 +79,11 @@ checker.prototype.changeCoord = function(X,Y){
 }
 
 checker.prototype.checkIfKing = function () {
-	if(this.coordY == 8 && !this.king &&this.color == "white"){
+	if (this.coordY == 8 && !this.king &&this.color == "white"){
 		this.king = true;
 		this.id.style.border = "4px solid #ff000";
 	}
-	if(this.coordY == 1 && !this.king &&this.color == "black"){
+	if (this.coordY == 1 && !this.king &&this.color == "black"){
 		this.king = true;
 		this.id.style.border = "4px solid #FFFF00";
 	}
@@ -141,33 +141,33 @@ the_checker = w_checker;
 function showMoves (piece) {
 	var match = false;
 	mustAttack = false;
-	if(selectedPiece){
+	if (selectedPiece){
 			erase_roads(selectedPiece);
 	}
 	selectedPiece = piece;
 	var i,j; // retine indicele damei
 	for ( j = 1; j <= 12; j++){
-		if(the_checker[j].id == piece){
+		if (the_checker[j].id == piece){
 			i = j;
 			selectedPieceindex = j;
 			match = true;
 		}
 	}
 
-	if(oneMove && !attackMoves(oneMove)){
+	if (oneMove && !attackMoves(oneMove)){
 		changeTurns(oneMove);
 		oneMove = undefined;
 		return false;
 	}
-	if(oneMove && oneMove != the_checker[i] ){
+	if (oneMove && oneMove != the_checker[i] ){
 		return false;
 	}
 
-	if(!match) {
+	if (!match) {
 	 return 0 ; 
 	}
 
-	if(the_checker[i].color =="white"){
+	if (the_checker[i].color =="white"){
 		tableLimit = 8;
 		tableLimitRight = 1;
 		tableLimitLeft = 8;
@@ -190,15 +190,15 @@ function showMoves (piece) {
 		attackMoves(the_checker[i]); 
 	
 
- 	if(!mustAttack){
+ 	if (!mustAttack){
  	  downLeft = checkMove( the_checker[i] , tableLimit , tableLimitRight , moveUpRight , downLeft);
 		downRight = checkMove( the_checker[i] , tableLimit , tableLimitLeft , moveUpLeft , downRight);
-		if(the_checker[i].king){
+		if (the_checker[i].king){
 			upLeft = checkMove( the_checker[i] , reverse_tableLimit , tableLimitRight , moveDownRight , upLeft);
 			upRight = checkMove( the_checker[i], reverse_tableLimit , tableLimitLeft , moveDownLeft, upRight)
 		}
 	}
-	if(downLeft || downRight || upLeft || upRight){
+	if (downLeft || downRight || upLeft || upRight){
 			return true;
 		}
 	return false;
@@ -207,23 +207,23 @@ function showMoves (piece) {
 
 
 function erase_roads(piece){
-	if(downRight) block[downRight].id.style.background = "#333";
-	if(downLeft) block[downLeft].id.style.background = "#333";
-	if(upRight) block[upRight].id.style.background = "#333";
-	if(upLeft) block[upLeft].id.style.background = "#333";
+	if (downRight) block[downRight].id.style.background = "#333";
+	if (downLeft) block[downLeft].id.style.background = "#333";
+	if (upRight) block[upRight].id.style.background = "#333";
+	if (upLeft) block[upLeft].id.style.background = "#333";
 }
 		
 function makeMove (index) {
 	var isMove = false;
-	if(!selectedPiece) 
+	if (!selectedPiece) 
 		return false;
-	if(index != upLeft && index != upRight && index != downLeft && index != downRight){
+	if (index != upLeft && index != upRight && index != downLeft && index != downRight){
 		erase_roads(0);
 		selectedPiece = undefined;
 		return false;
 	}
 
-	if(the_checker[1].color=="white"){
+	if (the_checker[1].color=="white"){
 		cpy_downRight = upRight;
 		cpy_downLeft = upLeft;
 		cpy_upLeft = downLeft;
@@ -236,59 +236,59 @@ function makeMove (index) {
 		cpy_upRight = downLeft;
 	}  
 
-	if(mustAttack)  
+	if (mustAttack)  
 		multiplier = 2;
 	else
 		multiplier = 1;
 
-		if(index == cpy_upRight){
+		if (index == cpy_upRight){
 			isMove = true;		
-			if(the_checker[1].color=="white"){
+			if (the_checker[1].color=="white"){
 				executeMove( multiplier * 1, multiplier * 1, multiplier * 9 );
-				if(mustAttack) eliminateCheck(index - 9);
+				if (mustAttack) eliminateCheck(index - 9);
 			}
 			else{
 				executeMove( multiplier * 1, multiplier * -1, multiplier * -7);
-				if(mustAttack) eliminateCheck( index + 7 );
+				if (mustAttack) eliminateCheck( index + 7 );
 			}
 		}
 
-		if(index == cpy_upLeft){
+		if (index == cpy_upLeft){
 	
 			isMove = true;
-			if (the_checker[1].color=="white"){
+			if  (the_checker[1].color=="white"){
 				executeMove( multiplier * -1, multiplier * 1, multiplier * 7);
-				if (mustAttack)	eliminateCheck(index - 7 );				
+				if  (mustAttack)	eliminateCheck(index - 7 );				
 			}
 			else {
 				executeMove( multiplier * -1, multiplier * -1, multiplier * -9);
-				if (mustAttack) eliminateCheck( index + 9 );
+				if  (mustAttack) eliminateCheck( index + 9 );
 			}
 		}
 
-		if (the_checker[selectedPieceindex].king){
+		if  (the_checker[selectedPieceindex].king){
 
-			if (index == cpy_downRight){
+			if  (index == cpy_downRight){
 				isMove = true;
-				if (the_checker[1].color=="white"){
+				if  (the_checker[1].color=="white"){
 					executeMove( multiplier * 1, multiplier * -1, multiplier * -7);
-					if (mustAttack) eliminateCheck ( index  + 7) ;
+					if  (mustAttack) eliminateCheck ( index  + 7) ;
 				}
 				else {
 					executeMove( multiplier * 1, multiplier * 1, multiplier * 9);
-					if (mustAttack) eliminateCheck ( index  - 9) ;
+					if  (mustAttack) eliminateCheck ( index  - 9) ;
 				}
 			}
 
-		if (index == cpy_downLeft){
+		if  (index == cpy_downLeft){
 			isMove = true;
-				if (the_checker[1].color=="white"){
+				if  (the_checker[1].color=="white"){
 					executeMove( multiplier * -1, multiplier * -1, multiplier * -9);
-					if (mustAttack) eliminateCheck ( index  + 9) ;
+					if  (mustAttack) eliminateCheck ( index  + 9) ;
 				}
 				else {
 					executeMove( multiplier * -1, multiplier * 1, multiplier * 7);
-					if (mustAttack) eliminateCheck ( index  - 7) ;
+					if  (mustAttack) eliminateCheck ( index  - 7) ;
 				}
 			}
 		}
@@ -296,13 +296,13 @@ function makeMove (index) {
 	erase_roads(0);
 	the_checker[selectedPieceindex].checkIfKing();
 
-	if (isMove) {
+	if  (isMove) {
 		playSound(moveSound);
 		anotherMove = undefined;
-		if (mustAttack) {
+		if  (mustAttack) {
 		anotherMove = attackMoves(the_checker[selectedPieceindex]);
 		}
-		if (anotherMove){
+		if  (anotherMove){
 			oneMove = the_checker[selectedPieceindex];
 			showMoves(oneMove);
 		}
@@ -310,9 +310,9 @@ function makeMove (index) {
 			oneMove = undefined;
 			changeTurns(the_checker[1]);
 			gameOver = checkIfLost();
-			if(gameOver) { setTimeout( declareWinner(),3000 ); return false};
+			if (gameOver) { setTimeout( declareWinner(),3000 ); return false};
 			gameOver = checkForMoves();
-			if(gameOver) { setTimeout( declareWinner() ,3000) ; return false};
+			if (gameOver) { setTimeout( declareWinner() ,3000) ; return false};
 		}
 	}
 }
@@ -329,8 +329,8 @@ function executeMove (X,Y,nSquare){
 }
 
 function checkMove(Apiece,tLimit,tLimit_Side,moveDirection,theDirection){
-	if(Apiece.coordY != tLimit){
-		if(Apiece.coordX != tLimit_Side && !block[ Apiece.ocupied_square + moveDirection ].ocupied){
+	if (Apiece.coordY != tLimit){
+		if (Apiece.coordX != tLimit_Side && !block[ Apiece.ocupied_square + moveDirection ].ocupied){
 			block[ Apiece.ocupied_square + moveDirection ].id.style.background = "#f00";
 			theDirection = Apiece.ocupied_square + moveDirection;
 		}
@@ -345,7 +345,7 @@ function checkMove(Apiece,tLimit,tLimit_Side,moveDirection,theDirection){
 
 
 function  checkAttack( check , X, Y , negX , negY, squareMove, direction){
-	if(check.coordX * negX >= 	X * negX && check.coordY *negY <= Y * negY && block[check.ocupied_square + squareMove ].ocupied && block[check.ocupied_square + squareMove].pieceId.color != check.color && !block[check.ocupied_square + squareMove * 2 ].ocupied){
+	if (check.coordX * negX >= 	X * negX && check.coordY *negY <= Y * negY && block[check.ocupied_square + squareMove ].ocupied && block[check.ocupied_square + squareMove].pieceId.color != check.color && !block[check.ocupied_square + squareMove * 2 ].ocupied){
 		mustAttack = true;
 		direction = check.ocupied_square +  squareMove*2 ;
 		block[direction].id.style.background = "#f00";
@@ -356,26 +356,24 @@ function  checkAttack( check , X, Y , negX , negY, squareMove, direction){
 		return direction;
 }
 
-function eliminateCheck(indexx){
-	if(indexx < 1 || indexx > 64)
+function eliminateCheck(indexx) {
+	if (indexx < 1 || indexx > 64)
 		return  0;
-
 	var x =block[ indexx ].pieceId ;
 	x.alive =false;
 	block[ indexx ].ocupied = false;
 	x.id.style.display  = "none";
 }
 
- 	
-function attackMoves(ckc){
 
- 		upRight = undefined;
- 		upLeft = undefined;
- 		downRight = undefined;
- 		downLeft = undefined;
+function attackMoves(ckc) {
+	upRight = undefined;
+	upLeft = undefined;
+	downRight = undefined;
+	downLeft = undefined;
 
- 	if(ckc.king ){
- 		if(ckc.color == "white"){
+	if (ckc.king ){
+		if (ckc.color == "white"){
 			upRight = checkAttack( ckc , 6, 3 , -1 , -1 , -7, upRight );
 			upLeft = checkAttack( ckc, 3 , 3 , 1 , -1 , -9 , upLeft );
 		}
@@ -384,7 +382,7 @@ function attackMoves(ckc){
 			downRight = checkAttack( ckc , 6 , 6 , -1, 1 ,9 , downRight );		
 		}
 	}
-	if(ckc.color == "white"){
+	if (ckc.color == "white"){
 	 	downLeft = checkAttack( ckc , 3, 6, 1 , 1 , 7 , downLeft );
 		downRight = checkAttack( ckc , 6 , 6 , -1, 1 ,9 , downRight );
 	}
@@ -392,33 +390,28 @@ function attackMoves(ckc){
 		upRight = checkAttack( ckc , 6, 3 , -1 , -1 , -7, upRight );
 		upLeft = checkAttack( ckc, 3 , 3 , 1 , -1 , -9 , upLeft );
 	}
- 	
- 	if(ckc.color== "black" && (upRight || upLeft || downLeft || downRight ) ) {
-	 	var p = upLeft;
-	 	upLeft = downLeft;
-	 	downLeft = p;
-
-	 	p = upRight;
-	 	upRight = downRight;
-	 	downRight = p;
-
-	 	p = downLeft ;
-	 	downLeft = downRight;
-	 	downRight = p;
-
-	 	p = upRight ;
-	 	upRight = upLeft;
-	 	upLeft = p;
- 	}
- 	if(upLeft != undefined || upRight != undefined || downRight != undefined || downLeft != undefined){
- 		return true;
-
- 	}
- 	return false;
+	if (ckc.color== "black" && (upRight || upLeft || downLeft || downRight ) ) {
+		var p = upLeft;
+		upLeft = downLeft;
+		downLeft = p;
+		p = upRight;
+		upRight = downRight;
+		downRight = p;
+		p = downLeft ;
+		downLeft = downRight;
+		downRight = p;
+		p = upRight ;
+		upRight = upLeft;
+		upLeft = p;
+	}
+	if (upLeft != undefined || upRight != undefined || downRight != undefined || downLeft != undefined){
+		return true;
+	}
+	return false;
 }
 
 function changeTurns(ckc){
-		if(ckc.color=="white")
+		if (ckc.color=="white")
 	the_checker = b_checker;
 else
 	the_checker = w_checker;
@@ -427,7 +420,7 @@ else
 function checkIfLost(){
 	var i;
 	for(i = 1 ; i <= 12; i++)
-		if(the_checker[i].alive)
+		if (the_checker[i].alive)
 			return false;
 	return true;
 }
@@ -435,7 +428,7 @@ function checkIfLost(){
 function  checkForMoves(){
 	var i ;
 	for(i = 1 ; i <= 12; i++)
-		if(the_checker[i].alive && showMoves(the_checker[i].id)){
+		if (the_checker[i].alive && showMoves(the_checker[i].id)){
 			erase_roads(0);
 			return false;
 		}
@@ -449,16 +442,16 @@ function declareWinner(){
 	if (the_checker[1].color == "white") {
 		score.innerHTML = "Black wins";
 	} else {
-		score.innerHTML = "Red wins";
+		score.innerHTML = "White wins";
 	}
 }
 
 function playSound(sound){
-	if(sound) sound.play();
+	if (sound) sound.play();
 }
 
 
-function getDimension (){
+function getDimension(){
 	contor ++;
  windowHeight = window.innerHeight
 	|| document.documentElement.clientHeight
@@ -484,7 +477,7 @@ if (windowWidth > 650){
 		if (bigScreen == -1) bigScreen = 1;
 	}
 
-	if (bigScreen !=cpy_bigScreen){
+	if (bigScreen != cpy_bigScreen){
 		for (var i = 1; i <= 12; i++){
 			b_checker[i].setCoord(0,0);
 			w_checker[i].setCoord(0,0);
