6356de5cb79196368301d900f039de6641b9a775
rsedlr <30870094+rsedlr@users.noreply.github.com>
2019-08-30 11:11:41 +0100

    anims enabled class removed

--- web/main.js
+++ web/main.js
@@ -16,7 +16,7 @@ var player;  // current player
 var move;  // stores a reference to the timeout function which allows it to be cleared on new game
 var isMac = navigator.platform.toUpperCase().indexOf('MAC') >= 0;  // maybe
 
-if (getCookie('anims') == 'False') {
+if (getCookie('anims') == 'True') {
 	anims() ;
 }
 
