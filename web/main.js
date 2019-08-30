54d384c97985a467da1a47af7542146faaa51784
rsedlr <30870094+rsedlr@users.noreply.github.com>
2019-08-30 11:14:02 +0100

    reverted

--- web/main.js
+++ web/main.js
@@ -11,12 +11,12 @@ var colDrop = document.getElementById("cols");
 var gameMode = 1;  // 0 - AIvsAI, 1 - 1player, 2 - 2player 
 var passCount = 0;  // how many consecutive passes have occured
 var undoable = false;  // player cannot undo as no move has been made
-var animations = false;
+var animations = true;
 var player;  // current player
 var move;  // stores a reference to the timeout function which allows it to be cleared on new game
 var isMac = navigator.platform.toUpperCase().indexOf('MAC') >= 0;  // maybe
 
-if (getCookie('anims') == 'True') {
+if (getCookie('anims') == 'False') {
 	anims() ;
 }
 
